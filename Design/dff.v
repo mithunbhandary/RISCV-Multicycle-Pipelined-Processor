9d5bf964ed9ecbe76828a2ad109e2f2e73abc7af
Mithun Bhandary <136704794+mithunbhandary@users.noreply.github.com>
2024-09-11 12:42:04 +0530

    Update dff.v by Mithun

--- Design/dff.v
+++ Design/dff.v
@@ -1,9 +1,9 @@
 module dff #(parameter WIDTH = 32) (
     input clk,
     input [WIDTH-1:0] in,
-    output reg [WIDTH-1:0] pipelined_out
+    output reg [WIDTH-1:0] pipelined_out  //used for pipelining
 );
     always @(posedge clk) begin
-        pipelined_out<=in;
+        pipelined_out<=in;   
     end
 endmodule
