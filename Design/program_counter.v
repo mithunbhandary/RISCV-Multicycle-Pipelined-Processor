095a9c27e20fc4049a79075b8c8dc7faad9ac6f4
Mithun Bhandary <136704794+mithunbhandary@users.noreply.github.com>
2024-09-11 12:18:05 +0530

    Update program_counter.v by Mithun

--- Design/program_counter.v
+++ Design/program_counter.v
@@ -1,1 +1,16 @@
-//pc code
+module program_counter #(parameter addr_width=7)(
+    input clk,
+    input reset,
+    output reg [addr_width-1:0] address
+);
+    wire [addr_width-1:0] pc_incremented=address+1; //incrementing PC
+    
+    always@(posedge clk) begin
+        if (reset) begin
+            address<={addr_width{1'b0}};  //resets pc to 0 the 1st position in the program memory
+        end
+        else begin
+            address<=pc_incremented; //updates pc with next value
+        end
+    end
+endmodule
