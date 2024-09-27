da73b1a98933817e80a1b371566d5fb1c00d141b
Rameez-Jaman <139850256+Rameez-Jaman@users.noreply.github.com>
2024-09-27 12:30:11 +0530

    Update cpu_riscv_tb.v

--- Testbench/cpu_riscv_tb.v
+++ Testbench/cpu_riscv_tb.v
@@ -12,18 +12,20 @@ module cpu_riscv_tb;
 		.reset(reset),
 		.pipelined_alu_out(pipelined_alu_out)
 	);
-   always #5 clk = ~clk;
+	
+	always #5 clk = ~clk;
+	
 	initial begin
 		// Initialize Inputs
 		clk = 0;
 		reset = 1;
 		#10 reset = 0;
-    #1000;
+		#1000;
 	end     
 
 	// Dump waveform for viewing
-  initial begin
-    $dumpfile("dump.vcd"); 
-    $dumpvars;
-  end
+	initial begin
+		$dumpfile("dump.vcd"); 
+		$dumpvars;
+	end
 endmodule
